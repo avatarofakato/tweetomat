20c8f67ede9a2fb6f02a3729b0b4f83c4ff3bbf8
Adam Baranowski <adam.baranowski1@gazeta.pl>
2014-02-19 17:28:51 +0100

    algorithm

---
+++ algorithm.ml
@@ -0,0 +1,8 @@
+let fondamental retweets favorites =
+	retweets + favorites
+
+let favoris retweets favorites =
+	2 * favorites + retweets
+
+let repondre retweets favorites =
+	favorites + 2 * retweets
